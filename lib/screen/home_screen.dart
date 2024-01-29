adbf09c67c187b3e0d6d8c26c548d68c9d01d52e
jmcunst <xnslqjtmghaf@gmail.com>
2024-01-29 14:03:28 +0900

    feat : stream provider

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:riverpod_ex/layout/default_layout.dart';
 import 'package:riverpod_ex/screen/state_notifier_provider_screen.dart';
 import 'package:riverpod_ex/screen/state_provider_screen.dart';
+import 'package:riverpod_ex/screen/stream_provider_screen.dart';
 
 import 'future_provider_screen.dart';
 
@@ -22,7 +23,10 @@ class HomeScreen extends StatelessWidget {
             }, child: Text('StateNotifierProviderScreen')),
             ElevatedButton(onPressed: () {
               Navigator.of(context).push(MaterialPageRoute(builder: (_) => FutureProviderScreen()));
-            }, child: Text('FutureProviderScreen'))
+            }, child: Text('FutureProviderScreen')),
+            ElevatedButton(onPressed: () {
+              Navigator.of(context).push(MaterialPageRoute(builder: (_) => StreamProviderScreen()));
+            }, child: Text('StreamProviderScreen'))
           ],
         ));
   }
